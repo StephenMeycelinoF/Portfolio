5173dbd1a1484170c57dc781167f499918719fff
Stephen Meycelino Fransisco <fstephenmeycelino@gmail.com>
2024-07-04 16:23:21 +0700

    feat: change logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Stephen Portfolio",
   description: "Modern & Minimalist Portfolio",
+  icons: { icon: "/logo.png" },
 };
 
 export default function RootLayout({
